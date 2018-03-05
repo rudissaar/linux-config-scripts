7ae122db85794b4e1b9d83b572e1b9a00974667f
Kevin Rudissaar <kevin.rudissaar@gmail.com>
2018-03-05 14:40:21 +0000

    Improved debian-post-install/enable-colours.sh script.

--- debian-post-install/enable-colours.sh
+++ debian-post-install/enable-colours.sh
@@ -1,5 +1,11 @@
 #!/usr/bin/env bash
 
+# You need root permissions to run this script.
+if [[ "${UID}" != '0' ]]; then
+    echo '> You need to become root to run this script.'
+    exit 1
+fi
+
 # Check if dircolors is installed.
 which dircolors 1>/dev/null 2>&1
 
@@ -9,10 +15,10 @@ if [[ "${?}" != '0' ]]; then
     apt-get install -y coreutils
 fi
 
-grep -Fq "LS_OPTIONS='--color=auto'" /etc/bash.bashrc
+grep -Fq "export LS_OPTIONS='--color=auto'" /etc/bash.bashrc
 
 if [[ "${?}" != '0' ]]; then
-    echo "LS_OPTIONS='--color=auto'" >> /etc/bash.bashrc
+    echo "export LS_OPTIONS='--color=auto'" >> /etc/bash.bashrc
 fi
 
 grep -Fq "eval \$(dircolors)" /etc/bash.bashrc
