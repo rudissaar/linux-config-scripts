e525562ac7f1399ecda87c5a3863045b954d478b
Kevin Rudissaar <kevin.rudissaar@gmail.com>
2018-05-12 22:52:53 +0000

    fedora-post-install/openvpn.sh script development.

--- fedora-post-install/openvpn.sh
+++ fedora-post-install/openvpn.sh
@@ -1,10 +1,12 @@
 #!/usr/bin/env bash
 
+OPENVPN_SERVER_DIR='/etc/openvpn/server'
+
 GATEWAY_INTERFACE=''
 OPENVPN_NETWORK='10.8.0.0'
 OPENVPN_NETMASK='255.255.255.0'
 OPENVPN_PROTOCOL='udp'
-OPENVPN_PORT=1195
+OPENVPN_PORT=1194
 
 USE_SAME_NAMESERVERS_AS_HOST=0
 NAMESERVER_1='8.8.8.8'
@@ -27,78 +29,74 @@ fi
 dnf update -y
 dnf install -y openvpn easy-rsa openssl net-tools sed
 
-# Make sure /etc/openvpn directory exists.
-if [[ ! -d /etc/openvpn ]]; then
-    mkdir /etc/openvpn
+# Make sure OpenVPN server directory exists.
+if [[ ! -d "${OPENVPN_SERVER_DIR}" ]]; then
+    mkdir -p "${OPENVPN_SERVER_DIR}"
 fi
-
-# Remove useless folders if they exist and are empty.
-rmdir /etc/openvpn/server 1> /dev/null 2>&1
-rmdir /etc/openvpn/client 1> /dev/null 2>&1
 
 # Move example configuration file to OpenVPN's directory.
-cp /usr/share/doc/openvpn/sample/sample-config-files/server.conf /etc/openvpn/
+cp /usr/share/doc/openvpn/sample/sample-config-files/server.conf "${OPENVPN_SERVER_DIR}"
 
 # Copy Easy RSA files to OpenVPN directory.
-if [[ ! -d /etc/openvpn/easy-rsa ]]; then
-    mkdir /etc/openvpn/easy-rsa
+if [[ ! -d "${OPENVPN_SERVER_DIR}/easy-rsa" ]]; then
+    mkdir -p "${OPENVPN_SERVER_DIR}/easy-rsa"
 fi
 
-cp -r /usr/share/easy-rsa/3/* /etc/openvpn/easy-rsa/
-cp /usr/share/doc/easy-rsa/vars.example /etc/openvpn/easy-rsa/vars
+cp -r /usr/share/easy-rsa/3/* "${OPENVPN_SERVER_DIR}/easy-rsa/"
+cp /usr/share/doc/easy-rsa/vars.example "${OPENVPN_SERVER_DIR}/easy-rsa/vars"
 
 # Edit vars with your default text editor, using vi as fallback.
 if [[ "${EDIT_VARS}" = '1' ]]; then
-    ${EDITOR:-vi} /etc/openvpn/easy-rsa/vars
+    ${EDITOR:-vi} "${OPENVPN_SERVER_DIR}/easy-rsa/vars"
 fi
 
 # Generate keys.
-cd /etc/openvpn/easy-rsa
+cd "${OPENVPN_SERVER_DIR}/easy-rsa"
 ./easyrsa init-pki
-touch /etc/openvpn/easy-rsa/pki/index.txt.attr
+touch "${OPENVPN_SERVER_DIR}/easy-rsa/pki/index.txt.attr"
 
 # Generate new Diffie-Hellman key.
 ./easyrsa gen-dh
-mv /etc/openvpn/easy-rsa/keys/dh.pem /etc/openvpn/
+mv "${OPENVPN_SERVER_DIR}/easy-rsa/pki/dh.pem" "${OPENVPN_SERVER_DIR}/dh2048.pem"
 
 # Generate CA key.
 echo | ./easyrsa build-ca nopass
-cp /etc/openvpn/easy-rsa/pki/ca.crt /etc/openvpn/
+cp "${OPENVPN_SERVER_DIR}/easy-rsa/pki/ca.crt" "${OPENVPN_SERVER_DIR}"
 
 # Generate Server key.
 ./easyrsa build-server-full server nopass
-mv /etc/openvpn/easy-rsa/pki/issued/server.crt /etc/openvpn/
-mv /etc/openvpn/easy-rsa/pki/private/server.key /etc/openvpn/
+mv "${OPENVPN_SERVER_DIR}/easy-rsa/pki/issued/server.crt" "${OPENVPN_SERVER_DIR}"
+mv "${OPENVPN_SERVER_DIR}/easy-rsa/pki/private/server.key" "${OPENVPN_SERVER_DIR}"
 
 # Generate crl.pem file.
 if [[ "${CRL_VERIFY}" = '1' ]]; then
-    if [[ ! -f /etc/openvpn/easy-rsa/pki/crl.pem ]]; then
+    if [[ ! -f "${OPENVPN_SERVER_DIR}/easy-rsa/pki/crl.pem" ]]; then
 	./easyrsa gen-crl
     fi
 fi
 
 # Generate TLS Auth key.
-openvpn --genkey --secret /etc/openvpn/ta.key
+openvpn --genkey --secret "${OPENVPN_SERVER_DIR}/ta.key"
 
 cd - 1> /dev/null
 
 # Change Port if you specified new one.
 if [[ "${OPENVPN_PORT}" != '1194' ]]; then
-    sed -i '/^port 1194/s/1194/'${OPENVPN_PORT}'/' /etc/openvpn/server.conf
+    sed -i '/^port 1194/s/1194/'${OPENVPN_PORT}'/' "${OPENVPN_SERVER_DIR}/server.conf"
 fi
 
 # Change Network if you specified new one.
 if [[ "${OPENVPN_NETWORK}" != '10.8.0.0' ]]; then
-    sed -i '/^server 10.8.0.0/s/10.8.0.0/'${OPENVPN_NETWORK}'/' /etc/openvpn/server.conf
+    sed -i '/^server 10.8.0.0/s/10.8.0.0/'${OPENVPN_NETWORK}'/' "${OPENVPN_SERVER_DIR}/server.conf"
 fi
 
 # Change Netmask if you specified new one.
 if [[ "${OPENVPN_NETMASK}" != '255.255.255.0' ]]; then
-    sed -i '/^server '${OPENVPN_NETWORK}' 255.255.255.0/s/ 255.255.255.0/ '${OPENVPN_NETMASK}'/' /etc/openvpn/server.conf
+    sed -i '/^server '${OPENVPN_NETWORK}' 255.255.255.0/s/ 255.255.255.0/ '${OPENVPN_NETMASK}'/' "${OPENVPN_SERVER_DIR}/server.conf"
 fi
 
 # Uncomment redirect-gateway line.
-sed -i '/;push "redirect-gateway def1 bypass-dhcp"/s/^;//g' /etc/openvpn/server.conf
+sed -i '/;push "redirect-gateway def1 bypass-dhcp"/s/^;//g' "${OPENVPN_SERVER_DIR}/server.conf"
 
 # Try to fetch nameservers from /etc/resolv.conf
 if [[ "${USE_SAME_NAMESERVERS_AS_HOST}" = '1' ]]; then
@@ -115,27 +113,27 @@ if [[ "${USE_SAME_NAMESERVERS_AS_HOST}" = '1' ]]; then
 fi
 
 # Uncomment and set DNS servers.
-sed -i 's/^;push "dhcp-option DNS .*/push "dhcp-option DNS '${NAMESERVER_2}'"/' /etc/openvpn/server.conf
-sed -i -r '0,/dhcp-option DNS '${NAMESERVER_2}'/s/'${NAMESERVER_2}'/'${NAMESERVER_1}'/' /etc/openvpn/server.conf
+sed -i 's/^;push "dhcp-option DNS .*/push "dhcp-option DNS '${NAMESERVER_2}'"/' "${OPENVPN_SERVER_DIR}/server.conf"
+sed -i -r '0,/dhcp-option DNS '${NAMESERVER_2}'/s/'${NAMESERVER_2}'/'${NAMESERVER_1}'/' "${OPENVPN_SERVER_DIR}/server.conf"
 
 # Enable CRL (Certificate Revocation List).
 if [[ "${CRL_VERIFY}" = '1' ]]; then
-    grep -Fq 'crl-verify' /etc/openvpn/server.conf
+    grep -Fq 'crl-verify' "${OPENVPN_SERVER_DIR}/server.conf"
 
     if [[ "${?}" != '0' ]]; then
-        echo -e "\n\n# Use certificate revocation list." >> /etc/openvpn/server.conf
-        echo 'crl-verify /etc/openvpn/easy-rsa/pki/crl.pem' >> /etc/openvpn/server.conf
+        echo -e "\n\n# Use certificate revocation list." >> "${OPENVPN_SERVER_DIR}/server.conf"
+        echo "crl-verify ${OPENVPN_SERVER_DIR}/easy-rsa/pki/crl.pem" >> "${OPENVPN_SERVER_DIR}/server.conf"
     fi
 fi
 
 # Enable LZO compression.
 if [[ "${LZO_COMPRESSION}" = '1' ]]; then
-    sed -i '/;comp-lzo/s/^;//g' /etc/openvpn/server.conf
+    sed -i '/;comp-lzo/s/^;//g' "${OPENVPN_SERVER_DIR}/server.conf"
 fi
 
 # Uncomment user and group lines.
-sed -i '/;user nobody/s/^;//g' /etc/openvpn/server.conf
-sed -i '/;group nobody/s/^;//g' /etc/openvpn/server.conf
+sed -i '/;user nobody/s/^;//g' "${OPENVPN_SERVER_DIR}/server.conf"
+sed -i '/;group nobody/s/^;//g' "${OPENVPN_SERVER_DIR}/server.conf"
 
 # Enable IPv4 forward if not enabled.
 echo -n 1 > /proc/sys/net/ipv4/ip_forward
@@ -149,3 +147,9 @@ grep -Fq 'net.ipv4.ip_forward=1' /etc/sysctl.conf
 if [[ "${?}" != '0' ]]; then
     echo 'net.ipv4.ip_forward=1' >> /etc/sysctl.conf
 fi
+
+# Enable OpenVPN service.
+#ln -s /lib/systemd/system/openvpn-server\@.service /etc/systemd/system/multi-user.target.wants/openvpn-server\@server.service
+#systemctl enable openvpn-server@server.service
+#systemctl start openvpn-server@server.service
+
