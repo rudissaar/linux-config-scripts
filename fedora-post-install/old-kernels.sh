270e4d7b084abcde5aedd42fb474dff237e822e9
Kevin Rudissaar <kevin.rudissaar@gmail.com>
2018-06-03 18:33:33 +0100

    Added fedora-post-install/old-kernels.sh script that you can run to remove old kernels.

---
+++ fedora-post-install/old-kernels.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+if [[ "${UID}" != '0' ]]; then
+    echo '> You need to become root to run this script.'
+    exit 1
+fi
+
+# Upgrade packages.
+dnf update -y
+
+# Install yum-utils to clean up kernels later.
+dnf install -y yum-utils
+
+# Remove old kernels.
+package-cleanup -y --oldkernels --count=1
+
+# Remove everything that is not used by any package.
+dnf autoremove -y
+
+# Remove repository's cache.
+dnf clean all
+
