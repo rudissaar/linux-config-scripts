b096c34e03a362a5a97bfd18e04dbc1499df2fda
Kevin Rudissaar <kevin.rudissaar@gmail.com>
2018-08-16 14:33:21 +0000

    Improved fedora-post-install/minetest-server.sh script, added mod option.

--- fedora-post-install/minetest-server.sh
+++ fedora-post-install/minetest-server.sh
@@ -2,6 +2,8 @@
 
 MINETEST_PORT=30000
 
+MINETEST_MOD_MOREBLOCKS=0
+
 RUN_FIREWALL_RULES=0
 
 # You need root permissions to run this script.
@@ -10,10 +12,37 @@ if [[ "${UID}" != '0' ]]; then
     exit 1
 fi
 
+GET_LATEST_RELEASE () {
+    URL="$(curl -s "https://api.github.com/repos/${1}/tags" | jq .[0].tarball_url | tr -d '"')"
+    DESTINATION="/tmp/$(echo "${1}" | tr '/' '-').tar.gz"
+    wget "${URL}" -O "${DESTINATION}"
+}
+
 # Install packages.
 dnf install -y \
 	minetest \
-	minetest-server
+	minetest-server \
+    curl \
+    wget \
+    jq
+
+# Install mods
+if [[ "${MINETEST_MOD_MOREBLOCKS}" == '1' ]]; then
+    MOD_REPO='minetest-mods/moreblocks'
+    GET_LATEST_RELEASE "${MOD_REPO}"
+    TARBALL="/tmp/$(echo ${MOD_REPO} | tr '/' '-').tar.gz"
+
+    if [[ -f "${TARBALL}" ]]; then
+        tar -xf "${TARBALL}" -C '/tmp'
+
+        if [[ ! -d '/usr/share/minetest/games/minetest_game/mods/moreblocks' ]]; then
+            mkdir -p '/usr/share/minetest/games/minetest_game/mods/moreblocks'
+        fi
+
+        cp -r "/tmp/$(echo ${MOD_REPO} | tr '/' '-')-"*/* /usr/share/minetest/games/minetest_game/mods/moreblocks/
+        rm -rf "/tmp/$(echo ${MOD_REPO} | tr '/' '-')"*
+    fi
+fi
 
 # Change Port if you specified new one.
 if [[ "${MINETEST_PORT}" != '30000' ]]; then
