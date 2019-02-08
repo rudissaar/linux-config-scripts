8cffb7733107f27f0c433502deb9a3658ad5800f
Kevin Rudissaar <kevin.rudissaar@gmail.com>
2019-02-08 18:42:33 +0000

    Added basic version of arch-post-install/hla.sh script.

---
+++ arch-post-install/hla.sh
@@ -0,0 +1,34 @@
+#!/usr/bin/env bash
+
+PACKAGE_POOL="/usr"
+
+ORIGINAL_URL="http://www.plantation-productions.com/Webster/HighLevelAsm/HLAv2.16/linux.hla.tar.gz"
+
+# You need root permissions to run this script.
+if [[ "${UID}" != '0' ]]; then
+    echo '> You need to become root to run this script.'
+    exit 1
+fi
+
+# Install packages.
+pacman --noconfirm -S coreutils wget
+
+# Download HLA archive.
+TMP_DATE="$(date +%s)"
+TMP_FILE="/tmp/hla-${TMP_DATE}.tar.gz"
+TMP_PATH="/tmp/hla-${TMP_DATE}"
+
+wget "${ORIGINAL_URL}" -O "${TMP_FILE}"
+
+# Extract archive.
+[[ -d "${TMP_PATH}" ]] || mkdir -p "${TMP_PATH}"
+tar -xf "${TMP_FILE}" --directory "${TMP_PATH}"
+
+# Copy files.
+cp -r "${TMP_PATH}/usr/"* "${PACKAGE_POOL}/"
+
+# Cleanup.
+rm -rf "${TMP_FILE}" "${TMP_PATH}"
+
+echo '> Finished.'
+
