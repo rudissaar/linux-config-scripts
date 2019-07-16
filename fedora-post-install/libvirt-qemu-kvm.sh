f04d8c60f1013ee60d2206a6647bf0669926cf56
Kevin Rudissaar <kevin.rudissaar@gmail.com>
2019-07-16 20:39:09 +0300

    Improvements to fedora-post-install/libvirt-qemu-kvm.sh script.

--- fedora-post-install/libvirt-qemu-kvm.sh
+++ fedora-post-install/libvirt-qemu-kvm.sh
@@ -1,5 +1,10 @@
 #!/usr/bin/env bash
 
+# Set this to 1 if you wish to remove all password requirements for org.libvirt.unix policy.
+# By doing this your system will be less secure due no authentication is required.
+POLKIT_NO_PASSWORD=0
+POLKIT_FILE='/usr/share/polkit-1/actions/org.libvirt.unix.policy'
+
 # You need root permissions to run this script.
 if [[ "${UID}" != '0' ]]; then
     echo '> You need to become root to run this script.'
@@ -17,6 +22,17 @@ dnf install -y \
 # Enable libvirt service.
 systemctl enable libvirtd
 systemctl restart libvirtd
+
+# Block that applies no password policy for org.libvirt.unix directive.
+if [[ "${POLKIT_NO_PASSWORD}" == '1' ]]; then
+    which sed 1> /dev/null 2>&1
+
+    if [[ "${?}" != '0' ]]; then
+        dnf install -y sed
+    fi
+
+    sed -i 's/auth_admin_keep/yes/g' "${POLKIT_FILE}"
+fi
 
 # Print info about KVM capabilities.
 virt-host-validate
