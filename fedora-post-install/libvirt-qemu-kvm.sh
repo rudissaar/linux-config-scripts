282e9e2fad4ff6cf7882d331af4196ff80dae74a
Kevin Rudissaar <kevin.rudissaar@gmail.com>
2019-07-17 21:08:13 +0300

    Added IP forwarding option for fedora-post-install/libvirt-qemu-kvm.sh script.

--- fedora-post-install/libvirt-qemu-kvm.sh
+++ fedora-post-install/libvirt-qemu-kvm.sh
@@ -5,6 +5,9 @@
 POLKIT_NO_PASSWORD=0
 POLKIT_FILE='/usr/share/polkit-1/actions/org.libvirt.unix.policy'
 
+# Just an extra measure to make sure that IPv4 forwarding gets enabled on host system.
+ENABLE_IPV4_FORWARDING=0
+
 # You need root permissions to run this script.
 if [[ "${UID}" != '0' ]]; then
     echo '> You need to become root to run this script.'
@@ -32,6 +35,31 @@ if [[ "${POLKIT_NO_PASSWORD}" == '1' ]]; then
     fi
 
     sed -i 's/auth_admin_keep/yes/g' "${POLKIT_FILE}"
+fi
+
+# IP forwarding.
+if [[ "${ENABLE_IPV4_FORWARDING}" == '1' ]]; then
+    echo -n 1 > /proc/sys/net/ipv4/ip_forward
+
+    which sed 1> /dev/null 2>&1
+
+    if [[ "${?}" != '0' ]]; then
+        dnf install -y sed
+    fi
+
+    sed -i '/#net.ipv4.ip_forward=1/s/^#//g' /etc/sysctl.conf
+
+    which grep 1> /dev/null 2>&1
+
+    if [[ "${?}" != '0' ]]; then
+        dnf install -y grep
+    fi
+
+    grep -Fq 'net.ipv4.ip_forward=1' /etc/sysctl.conf
+
+    if [[ "${?}" != '0' ]]; then
+        echo 'net.ipv4.ip_forward=1' >> /etc/sysctl.conf
+    fi
 fi
 
 # Print info about KVM capabilities.
