5a7f01ab2c6b6d48744e64b3bfbb29d239045f3e
Kevin Rudissaar <kevin.rudissaar@gmail.com>
2019-07-28 16:34:07 +0000

    Added basic fedora-post-install/apache-netbeans.sh script.

---
+++ fedora-post-install/apache-netbeans.sh
@@ -0,0 +1,57 @@
+#!/usr/bin/env bash
+
+PACKAGE_POOL="/usr/local"
+
+DOWNLOAD_EU_URL="https://www-eu.apache.org/dist/incubator/netbeans/incubating-netbeans/incubating-11.0/incubating-netbeans-11.0-bin.zip"
+DOWNLOAD_US_URL="https://www-us.apache.org/dist/incubator/netbeans/incubating-netbeans/incubating-11.0/incubating-netbeans-11.0-bin.zip"
+
+# You need root permissions to run this script.
+if [[ "${UID}" != '0' ]]; then
+    echo '> You need to become root to run this script.'
+    exit 1
+fi
+
+REQUIREMENTS=('wget' 'grep' 'unzip')
+
+# Check for requirements and install them if necessary.
+for REQUIREMENT in ${REQUIREMENTS}; do
+    which ${REQUIREMENT} 1> /dev/null 2>&1
+    [[ "${?}" == '0' ]] || dnf install -y ${REQUIREMENT}
+done
+
+# Download Apache NetBeans archive.
+TMP_DATE="$(date +%s)"
+TMP_FILE="/tmp/apache-netbeans-${TMP_DATE}.zip"
+TMP_PATH="/tmp/apache-netbeans-${TMP_DATE}"
+
+wget "${DOWNLOAD_EU_URL}" -O "${TMP_FILE}"
+
+if [[ "${?}" != '0' ]]; then
+    wget "${DOWNLOAD_US_URL}" -O "${TMP_FILE}"
+fi
+
+if [[ "${?}" != '0' ]]; then
+    echo '> Unable to download required files, exiting.'
+    exit 1
+fi
+
+# Extract archive.
+[[ -d "${TMP_PATH}" ]] || mkdir -p "${TMP_PATH}"
+unzip -q "${TMP_FILE}" -d "${TMP_PATH}"
+
+# Copy files.
+cp -r "${TMP_PATH}/"* "${PACKAGE_POOL}/share/"
+
+for BINARY in $(find "${PACKAGE_POOL}/share/netbeans/bin" -maxdepth 1 -type f -executable)
+do
+    ln -sf "${BINARY}" "${PACKAGE_POOL}/bin/$(basename ${BINARY})"
+done
+
+# Cleanup.
+rm -rf "${TMP_FILE}" "${TMP_PATH}"
+
+# Let user know that script has finished it's job.
+echo '> Finished.'
+
+exit
+
