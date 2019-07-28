dca6c3d6081c23ec4c8406d90740df04a90223c6
Kevin Rudissaar <kevin.rudissaar@gmail.com>
2019-07-28 17:13:17 +0000

    Improved fedora-post-install/selinux-off.sh script.

--- fedora-post-install/selinux-off.sh
+++ fedora-post-install/selinux-off.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
+# Script that disables SELinux on current system.
 
 # You need root permissions to run this script.
 if [[ "${UID}" != '0' ]]; then
@@ -9,17 +10,22 @@ fi
 # Check if SELinux is enabled.
 selinuxenabled
 
-# If it's already disabled then just exit.
-if [[ "${?}" != '0' ]]; then
-    exit
+# Perform actions.
+if [[ "${?}" == '0' ]]; then
+    # Disable SELinux in runtime.
+    echo -n 0 > /sys/fs/selinux/enforce
+
+    # Make sure that sed binary is installed, we'll need this on next step.
+    which sed 1> /dev/null 2>&1
+    [[ "${?}" == '0' ]] || dnf install -y sed
+
+    # Disable SELinux from config.
+    sed -i 's/SELINUX=enforcing/SELINUX=disabled/g' /etc/selinux/config
 fi
 
-# Disable SELinux in runtime.
-echo -n 0 > /sys/fs/selinux/enforce
-
-# Disable SELinux from config.
-sed -i 's/SELINUX=enforcing/SELINUX=disabled/g' /etc/selinux/config
-
 # Remove packages.
-dnf remove -y selinux*
+dnf remove -y selinux* setroubleshoot-server
+
+# Let user know that script has finished it's job.
+echo '> Finished.'
 
