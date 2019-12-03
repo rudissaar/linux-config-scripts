610db0087345a85340ae7c9f53627089df36e2ac
Kevin Rudissaar <kevin.rudissaar@gmail.com>
2019-12-03 20:40:39 +0000

    Improved fedora-post-install/pam-su-no-password.sh script.

--- fedora-post-install/pam-su-no-password.sh
+++ fedora-post-install/pam-su-no-password.sh
@@ -1,20 +1,37 @@
 #!/usr/bin/env bash
+# Script that allows wheel group users to use su command without entering password.
 
 PAM_SU_FILE='/etc/pam.d/su'
-REMOVE_INSTRUCTION_COMMENTS=0
+REMOVE_INSTRUCTION_COMMENTS=1
 
 # You need root permissions to run this script.
 if [[ "${UID}" != '0' ]]; then
     echo '> You need to become root to run this script.'
+    echo '> Aborting.'
     exit 1
 fi
 
-# Check if 'sed' is installed on system, if it's not then install it.
-which sed 1> /dev/null 2>&1
+# Function that checks if required binary exists and installs it if necassary.
+ENSURE_DEPENDENCY () {
+    REQUIRED_BINARY=$(basename "${1}")
+    REPO_PACKAGE="${2}"
+    [[ -n "${REPO_PACKAGE}" ]] || REPO_PACKAGE="${REQUIRED_BINARY}"
 
-if [[ "${?}" != '0' ]]; then
-    dnf install -y sed
-fi
+    if ! command -v "${REQUIRED_BINARY}" 1> /dev/null; then
+        if [[ "${REPO_UPDATED}" == '0' ]]; then
+            dnf check-update
+            REPO_UPDATED=1
+        fi
+
+        dnf install -y "${REPO_PACKAGE}"
+    fi
+}
+
+# Variable that keeps track if repository is already refreshed.
+REPO_UPDATED=0
+
+# Check if 'sed' is installed on system, if it's not then install it.
+ENSURE_DEPENDENCY 'sed'
 
 # Look for line with specific content and uncomment it if it's found.
 sed -i \
@@ -33,6 +50,6 @@ if [[ "${REMOVE_INSTRUCTION_COMMENTS}" == '1' ]]; then
         "${PAM_SU_FILE}"
 fi
 
-# Let user know that script has finished it's job.
+# Let user know that script has finished its job.
 echo '> Finished.'
 
