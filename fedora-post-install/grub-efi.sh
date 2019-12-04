5e437d208e91666a1907865db5c47b9f8c809354
Kevin Rudissaar <kevin.rudissaar@gmail.com>
2019-12-04 21:43:23 +0000

    Improved fedora-post-install/grub-efi.sh script.

--- fedora-post-install/grub-efi.sh
+++ fedora-post-install/grub-efi.sh
@@ -1,10 +1,15 @@
 #!/usr/bin/env bash
+# Script that regenerates grub efi config file.
 
 # You need root permissions to run this script.
 if [[ "${UID}" != '0' ]]; then
     echo '> You need to become root to run this script.'
+    echo '> Aborting.'
     exit 1
 fi
 
 grub2-mkconfig -o /boot/efi/EFI/fedora/grub.cfg
+
+# Let user know that script has finished its job.
+echo '> Finished.'
 
