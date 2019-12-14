b88f4ddc9372843b270efb508f09365762d28d3d
Kevin Rudissaar <kevin.rudissaar@gmail.com>
2019-12-14 15:06:05 +0000

    Improved rhel-post-install/vim.sh script.

--- rhel-post-install/vim.sh
+++ rhel-post-install/vim.sh
@@ -1,72 +1,99 @@
 #!/usr/bin/env bash
-# Script that install vim and generates customized .vimrc file.
+# Script that installs and configures vim editor on current system.
 
 OVERWRITE_USERS_VIMRC=1
 
 # You need root permissions to run this script.
 if [[ "${UID}" != '0' ]]; then
     echo '> You need to become root to run this script.'
+    echo '> Aborting.'
     exit 1
 fi
 
-# Install packages.
-yum install -y vim
+# Function that checks if required binary exists and installs it if necassary.
+ENSURE_DEPENDENCY () {
+    REQUIRED_BINARY=$(basename "${1}")
+    REPO_PACKAGE="${2}"
+    [[ -n "${REPO_PACKAGE}" ]] || REPO_PACKAGE="${REQUIRED_BINARY}"
+
+    if ! command -v "${REQUIRED_BINARY}" 1> /dev/null; then
+        if [[ "${REPO_UPDATED}" == '0' ]]; then
+            yum check-update
+            REPO_UPDATED=1
+        fi
+
+        yum install -y "${REPO_PACKAGE}"
+    fi
+}
+
+# Variable that keeps track if repository is already refreshed.
+REPO_UPDATED=0
+
+# Install dependencies if necassary.
+ENSURE_DEPENDENCY 'grep'
+ENSURE_DEPENDENCY 'awk' 'gawk'
+
+# Install packages if necessary.
+if ! command -v vim 1> /dev/null; then
+    if [[ ${REPO_UPDATED} -eq 0 ]]; then
+        yum check-update
+    fi
+
+    yum install -y vim
+fi
 
 # Replace vi command with vim.
-grep -Fq 'alias vi=' /etc/bashrc
-
-if [[ "${?}" != '0' ]]; then
+if ! grep -Fq 'alias vi=' /etc/bashrc; then
     echo >> /etc/bashrc
     echo "alias vi='vim'" >> /etc/bashrc
 fi
 
+# Create .vimrc file in /etc/skel directory.
 if [[ ! -f /etc/skel/.vimrc ]]; then
     touch /etc/skel/.vimrc
 fi
 
-# Enable syntax.
-grep -Fq 'syntax on' /etc/skel/.vimrc
+# Create .gvimrc file in /etc/skel directory.
+if [[ ! -f /etc/skel/.gvimrc ]]; then
+    touch /etc/skel/.gvimrc
+fi
 
-if [[ "${?}" != '0' ]]; then
+# Enable syntax.
+if ! grep -Fq 'syntax on' /etc/skel/.vimrc; then
     echo 'syntax on' >> /etc/skel/.vimrc
 fi
 
 # Display line numbers.
-grep -Fq 'set number' /etc/skel/.vimrc
-
-if [[ "${?}" != '0' ]]; then
+if ! grep -Fq 'set number' /etc/skel/.vimrc; then
     echo 'set number' >> /etc/skel/.vimrc
 fi
 
 # Disable visual mode.
-grep -Fq 'set mouse-=a' /etc/skel/.vimrc
-
-if [[ "${?}" != '0' ]]; then
+if ! grep -Fq 'set mouse-=a' /etc/skel/.vimrc; then
     echo 'set mouse-=a' >> /etc/skel/.vimrc
 fi
 
-# Sset size of tab.
-grep -Fq 'set tabstop=4' /etc/skel/.vimrc
+# Enable visual mode for gvim.
+if ! grep -Fq 'set mouse=a' /etc/skel/.gvimrc; then
+    echo 'set mouse=a' >> /etc/skel/.gvimrc
+fi
 
-if [[ "${?}" != '0' ]]; then
+# Set size of tab.
+if ! grep -Fq 'set tabstop=4' /etc/skel/.vimrc; then
     echo 'set tabstop=4' >> /etc/skel/.vimrc
 fi
 
 # Use spaces instead of tabs.
-grep -Fq 'set expandtab' /etc/skel/.vimrc
-
-if [[ "${?}" != '0' ]]; then
+if ! grep -Fq 'set expandtab' /etc/skel/.vimrc; then
     echo 'set expandtab' >> /etc/skel/.vimrc
 fi
 
 # Set size of indent.
-grep -Fq 'set shiftwidth=4' /etc/skel/.vimrc
-
-if [[ "${?}" != '0' ]]; then
+if ! grep -Fq 'set shiftwidth=4' /etc/skel/.vimrc; then
     echo 'set shiftwidth=4' >> /etc/skel/.vimrc
 fi
 
-# Copy .vimrc file to every user's home directory.
+# Block that populates user directories with generated .vimrc file.
 if [[ "${OVERWRITE_USERS_VIMRC}" = '1' ]]; then
     cp /etc/skel/.vimrc "${HOME}/.vimrc"
 
@@ -78,12 +105,14 @@ if [[ "${OVERWRITE_USERS_VIMRC}" = '1' ]]; then
         for USERNAME in ${USERNAMES}; do
             HOME_DIR=$(eval echo "~${USERNAME}")
 	        cp /etc/skel/.vimrc "${HOME_DIR}/.vimrc"
-            chown ${USERNAME}:${USERNAME} "${HOME_DIR}/.vimrc"
+	        cp /etc/skel/.gvimrc "${HOME_DIR}/.gvimrc"
+            chown "${USERNAME}:${USERNAME}" "${HOME_DIR}/.vimrc" "${HOME_DIR}/.gvimrc"
         done
     else
         echo '> Unable to overwrite .vimrc for normal users, missing file: "/etc/login.defs".'
     fi
 fi
 
+# Let user know that script has finished its job.
 echo '> Finished.'
 
