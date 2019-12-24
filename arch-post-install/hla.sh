54ff7bd3c5a9e4721c80c73dc024472b0fa9b108
Kevin Rudissaar <kevin.rudissaar@gmail.com>
2019-12-24 01:58:02 +0000

    Improved arch-post-install/hla.sh script.

--- arch-post-install/hla.sh
+++ arch-post-install/hla.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-# Script that installs environment for High Level Assembly developemnt.
+# Script that installs environment for High Level Assembly developement.
 
 PACKAGE_POOL="/usr"
 
@@ -9,30 +9,30 @@ FALLBACK_URL="http://legacy.murda.eu/downloads/misc/hla-linux.tar.gz"
 # You need root permissions to run this script.
 if [[ "${UID}" != '0' ]]; then
     echo '> You need to become root to run this script.'
+    echo '> Aborting.'
     exit 1
 fi
 
 # Install packages.
-which grep 1> /dev/null 2>&1
-[[ "${?}" == '0' ]] || pacman --noconfirm -S grep
+if ! command -v grep 1> /dev/null 2>&1; then
+    pacman --noconfirm -S grep
+fi
 
-which wget 1> /dev/null 2>&1
-[[ "${?}" == '0' ]] || pacman --noconfirm -S wget
+if ! command -v wget 1> /dev/null 2>&1; then
+    pacman --noconfirm -S wget
+fi
 
 # Download HLA archive.
 TMP_DATE="$(date +%s)"
 TMP_FILE="/tmp/hla-${TMP_DATE}.tar.gz"
 TMP_PATH="/tmp/hla-${TMP_DATE}"
 
-wget "${ORIGINAL_URL}" -O "${TMP_FILE}"
-
-if [[ "${?}" != '0' ]]; then
-    wget "${FALLBACK_URL}" -O "${TMP_FILE}"
-fi
-
-if [[ "${?}" != '0' ]]; then
-    echo '> Unable to download required files, exiting.'
-    exit 1
+if ! wget "${ORIGINAL_URL}" -O "${TMP_FILE}"; then
+    if ! wget "${FALLBACK_URL}" -O "${TMP_FILE}"; then
+        echo '> Unable to download required files, exiting.'
+        echo '> Aborting.'
+        exit 1
+    fi
 fi
 
 # Extract archive.
@@ -42,25 +42,24 @@ tar -xf "${TMP_FILE}" --directory "${TMP_PATH}"
 # Copy files.
 cp -r "${TMP_PATH}/usr/"* "${PACKAGE_POOL}/"
 
-for BINARY in $(find "${PACKAGE_POOL}/hla" -maxdepth 1 -type f -executable)
+while IFS= read -r -d '' BINARY
 do
-    ln -sf "${BINARY}" "${PACKAGE_POOL}/bin/$(basename ${BINARY})"
-done
+    BASENAME=$(basename "${BINARY}")
+    ln -sf "${BINARY}" "${PACKAGE_POOL}/bin/${BASENAME}"
+done < <(find "${PACKAGE_POOL}/hla" -maxdepth 1 -type f -executable -print0)
 
 # Setup global environment variables.
-grep -Fq 'export hlalib=' /etc/profile
-if [[ "${?}" != '0' ]]; then
+if ! grep -Fq 'export hlalib=' /etc/profile; then
     echo "export hlalib=${PACKAGE_POOL}/hla/hlalib" >> /etc/profile
 fi
 
-grep -Fq 'export hlainc=' /etc/profile
-if [[ "${?}" != '0' ]]; then
+if ! grep -Fq 'export hlainc=' /etc/profile; then
     echo "export hlainc=${PACKAGE_POOL}/hla/include" >> /etc/profile
 fi
 
 # Cleanup.
 rm -rf "${TMP_FILE}" "${TMP_PATH}"
 
-# Let user know that script has finished it's job.
+# Let user know that script has finished its job.
 echo '> Finished.'
 
