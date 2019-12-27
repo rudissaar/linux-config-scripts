4dd1eeeb05dacc37c30267f1e12bdc660e4c989a
Kevin Rudissaar <kevin.rudissaar@gmail.com>
2019-12-27 00:30:53 +0000

    Improved rhel-post-install/vim.sh script.

--- rhel-post-install/vim.sh
+++ rhel-post-install/vim.sh
@@ -11,42 +11,47 @@ if [[ "${UID}" != '0' ]]; then
 fi
 
 # Function that checks if required binary exists and installs it if necassary.
-ENSURE_DEPENDENCY () {
+ENSURE_PACKAGE () {
     REQUIRED_BINARY=$(basename "${1}")
-    REPO_PACKAGE="${2}"
-    [[ -n "${REPO_PACKAGE}" ]] || REPO_PACKAGE="${REQUIRED_BINARY}"
+    REPO_PACKAGES="${*:2}"
 
-    if ! command -v "${REQUIRED_BINARY}" 1> /dev/null; then
-        if [[ "${REPO_UPDATED}" == '0' ]]; then
-            yum check-update
-            REPO_UPDATED=1
+    if [[ "${REQUIRED_BINARY}" != '-' ]]; then
+        [[ -n "${REPO_PACKAGES}" ]] || REPO_PACKAGES="${REQUIRED_BINARY}"
+
+        if command -v "${REQUIRED_BINARY}" 1> /dev/null; then
+            REPO_PACKAGES=''
         fi
-
-        yum install -y "${REPO_PACKAGE}"
     fi
+
+    [[ -n "${REPO_PACKAGES}" ]] || return
+
+    if [[ "${REPO_REFRESHED}" == '0' ]]; then
+       yum check-update 1> /dev/null
+       REPO_REFRESHED=1
+   fi
+
+    for REPO_PACKAGE in ${REPO_PACKAGES}
+    do
+        yum install -y "${REPO_PACKAGE}"
+    done
 }
 
 # Variable that keeps track if repository is already refreshed.
-REPO_UPDATED=0
+REPO_REFRESHED=0
 
-# Install dependencies if necassary.
-ENSURE_DEPENDENCY 'grep'
-ENSURE_DEPENDENCY 'awk' 'gawk'
-
-# Install packages if necessary.
-if ! command -v vim 1> /dev/null; then
-    if [[ ${REPO_UPDATED} -eq 0 ]]; then
-        yum check-update
-    fi
-
-    yum install -y vim
-fi
+# Install packages and dependencies if necassary.
+ENSURE_PACKAGE 'grep'
+ENSURE_PACKAGE 'awk' 'gawk'
+ENSURE_PACKAGE 'vim'
 
 # Replace vi command with vim.
 if ! grep -Fq 'alias vi=' /etc/bashrc; then
     echo >> /etc/bashrc
     echo "alias vi='vim'" >> /etc/bashrc
 fi
+
+# Create /etc/skel directory if it doesn't exist.
+[[ -d '/etc/skel' ]] || mkdir -p '/etc/skel'
 
 # Create .vimrc file in /etc/skel directory.
 if [[ ! -f /etc/skel/.vimrc ]]; then
@@ -94,7 +99,7 @@ if ! grep -Fq 'set shiftwidth=4' /etc/skel/.vimrc; then
 fi
 
 # Block that populates user directories with generated .vimrc file.
-if [[ "${OVERWRITE_USERS_VIMRC}" = '1' ]]; then
+if [[ "${OVERWRITE_USERS_VIMRC}" == '1' ]]; then
     cp /etc/skel/.vimrc "${HOME}/.vimrc"
 
     if [[ -f /etc/login.defs ]]; then
