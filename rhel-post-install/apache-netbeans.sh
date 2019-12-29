3d90fb7a575e81bdded20de3714000077f557892
Kevin Rudissaar <kevin.rudissaar@gmail.com>
2019-12-29 06:38:48 +0000

    Improved rhel-post-install/apache-netbeans.sh script.

--- rhel-post-install/apache-netbeans.sh
+++ rhel-post-install/apache-netbeans.sh
@@ -16,37 +16,59 @@ fi
 # You need root permissions to run this script.
 if [[ "${UID}" != '0' ]]; then
     echo '> You need to become root to run this script.'
+    echo '> Aborting.'
     exit 1
 fi
 
-REQUIREMENTS=('wget' 'grep' 'unzip')
+# Function that checks if required binary exists and installs it if necessary.
+ENSURE_PACKAGE () {
+    REQUIRED_BINARY=$(basename "${1}")
+    REPO_PACKAGES="${*:2}"
 
-# Check for requirements and install them if necessary.
-for REQUIREMENT in ${REQUIREMENTS}; do
-    which ${REQUIREMENT} 1> /dev/null 2>&1
-    [[ "${?}" == '0' ]] || yum install -y ${REQUIREMENT}
-done
+    if [[ "${REQUIRED_BINARY}" != '-' ]]; then
+        [[ -n "${REPO_PACKAGES}" ]] || REPO_PACKAGES="${REQUIRED_BINARY}"
 
-which java 1> /dev/null 2>&1
-[[ "${?}" == '0' ]] || yum install -y java-latest-openjdk
+        if command -v "${REQUIRED_BINARY}" 1> /dev/null; then
+            REPO_PACKAGES=''
+        fi
+    fi
 
-which javac 1> /dev/null 2>&1
-[[ "${?}" == '0' ]] || yum install -y java-latest-openjdk-devel
+    [[ -n "${REPO_PACKAGES}" ]] || return
+
+    if [[ "${REPO_REFRESHED}" == '0' ]]; then
+        echo '> Refreshing package repository.'
+        yum check-update 1> /dev/null
+        REPO_REFRESHED=1
+    fi
+
+    for REPO_PACKAGE in ${REPO_PACKAGES}
+    do
+        yum install -y "${REPO_PACKAGE}"
+    done
+}
+
+# Variable that keeps track if repository is already refreshed.
+REPO_REFRESHED=0
+
+# Install packages and dependencies if necessary.
+ENSURE_PACKAGE 'wget'
+ENSURE_PACKAGE 'grep'
+ENSURE_PACKAGE 'unzip'
+ENSURE_PACKAGE 'find' 'findutils'
+ENSURE_PACKAGE 'java' 'java-latest-openjdk'
+ENSURE_PACKAGE 'javac' 'java-latest-openjdk-devel'
 
 # Download Apache NetBeans archive.
 TMP_DATE="$(date +%s)"
 TMP_FILE="/tmp/apache-netbeans-${TMP_DATE}.zip"
 TMP_PATH="/tmp/apache-netbeans-${TMP_DATE}"
 
-wget "${DOWNLOAD_EU_URL}" -O "${TMP_FILE}"
-
-if [[ "${?}" != '0' ]]; then
-    wget "${DOWNLOAD_US_URL}" -O "${TMP_FILE}"
-fi
-
-if [[ "${?}" != '0' ]]; then
-    echo '> Unable to download required files, exiting.'
-    exit 1
+if ! wget "${DOWNLOAD_EU_URL}" -O "${TMP_FILE}"; then
+    if ! wget "${DOWNLOAD_US_URL}" -O "${TMP_FILE}"; then
+        echo '> Unable to download required files, exiting.'
+        echo '> Aborting.'
+        exit 1
+    fi
 fi
 
 # Extract archive.
@@ -56,10 +78,11 @@ unzip -q "${TMP_FILE}" -d "${TMP_PATH}"
 # Copy files.
 cp -r "${TMP_PATH}/"* "${PACKAGE_POOL}/share/"
 
-for BINARY in $(find "${PACKAGE_POOL}/share/netbeans/bin" -maxdepth 1 -type f -executable)
+while IFS= read -r -d '' BINARY
 do
-    ln -sf "${BINARY}" "${PACKAGE_POOL}/bin/$(basename ${BINARY})"
-done
+    BASENAME=$(basename "${BINARY}")
+    ln -sf "${BINARY}" "${PACKAGE_POOL}/bin/${BASENAME}"
+done < <(find "${PACKAGE_POOL}/share/netbeans/bin" -maxdepth 1 -type f -executable -print0)
 
 # Create desktop entry for application.
 if [[ ! -f "${PACKAGE_POOL}/share/applications/apache-netbeans.desktop" ]]; then
