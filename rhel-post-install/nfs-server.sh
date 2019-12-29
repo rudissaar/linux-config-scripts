36f6326cd50cf3fddb596144fc7aa4f43250005f
Kevin Rudissaar <kevin.rudissaar@gmail.com>
2019-12-29 06:29:35 +0000

    Improved rhel-post-install/nfs-server.sh script.

--- rhel-post-install/nfs-server.sh
+++ rhel-post-install/nfs-server.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
+# Script that install publicly accessible NFS share on current system.
 
 SHARE_DIR='/share'
 SHARE_HOSTS='*'
@@ -9,32 +10,59 @@ RUN_FIREWALL_RULES=0
 # You need root permissions to run this script.
 if [[ "${UID}" != '0' ]]; then
     echo '> You need to become root to run this script.'
+    echo '> Aborting.'
     exit 1
 fi
 
-# Check if SeLinux is enabled on system.
+# Function that checks if required binary exists and installs it if necassary.
+ENSURE_PACKAGE () {
+    REQUIRED_BINARY=$(basename "${1}")
+    REPO_PACKAGES="${*:2}"
+
+    if [[ "${REQUIRED_BINARY}" != '-' ]]; then
+        [[ -n "${REPO_PACKAGES}" ]] || REPO_PACKAGES="${REQUIRED_BINARY}"
+
+        if command -v "${REQUIRED_BINARY}" 1> /dev/null; then
+            REPO_PACKAGES=''
+        fi
+    fi
+
+    [[ -n "${REPO_PACKAGES}" ]] || return
+
+    if [[ "${REPO_REFRESHED}" == '0' ]]; then
+        echo '> Refreshing package repository.'
+        yum check-update 1> /dev/null
+        REPO_REFRESHED=1
+    fi
+
+    for REPO_PACKAGE in ${REPO_PACKAGES}
+    do
+        yum install -y "${REPO_PACKAGE}"
+    done
+}
+
+# Variable that keeps track if repository is already refreshed.
+REPO_REFRESHED=0
+
+# Check if SELinux is enabled on system.
 SELINUX_ENABLED=0
-which selinuxenabled 1> /dev/null 2>&1
 
-if [[ "${?}" == '0' ]]; then
-    selinuxenabled
-
-    if [[ "${?}" == 0 ]]; then
+if command -v selinuxenabled 1> /dev/null 2>&1; then
+    if selinuxenabled; then
         SELINUX_ENABLED=1
     fi
 fi
 
-# Install packages.
-yum install -y \
-    nfs-utils \
-    nfs4-acl-tools
+# Install packages and dependencies if necessary.
+ENSURE_PACKAGE 'mount.nfs' 'nfs-utils'
+ENSURE_PACKAGE 'fs4_getfacl' 'nfs4-acl-tools'
 
 # Make sure NFS share directory exists.
 if [[ ! -d "${SHARE_DIR}" ]]; then
     mkdir -p "${SHARE_DIR}"
 fi
 
-# Apply SeLinux rules if necessary.
+# Apply SELinux rules if necessary.
 if [[ "${SELINUX_ENABLED}" == '1' ]]; then
     setsebool -P nfs_export_all_ro on
     setsebool -P nfs_export_all_rw on
@@ -49,9 +77,9 @@ EOL
 exportfs -avr
 
 # Active firewall rules.
-if [[ "${RUN_FIREWALL_RULES}" = '1' ]]; then
+if [[ "${RUN_FIREWALL_RULES}" == '1' ]]; then
     # Make sure firewalld is installed.
-    yum install -y firewalld
+    ENSURE_PACKAGE 'firewall-cmd' 'firewalld'
 
     # Enable Firewalld service.
     systemctl enable firewalld
@@ -76,4 +104,7 @@ systemctl restart rpcbind
 # Enable NFS service.
 systemctl enable nfs-server
 systemctl restart nfs-server
+
+# Let user know that script has finished its job.
+echo '> Finished.'
 
