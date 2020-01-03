19fb2fa82b22ec9bed08813ea34b3af3c5c361e7
Kevin Rudissaar <kevin.rudissaar@gmail.com>
2020-01-03 19:10:32 +0000

    Improved fedora-post-install/nfs-server.sh script.

--- fedora-post-install/nfs-server.sh
+++ fedora-post-install/nfs-server.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
+# Script that install publicly accessible NFS share on current system.
 
 SHARE_DIR='/data'
 SHARE_HOSTS='*'
@@ -9,25 +10,52 @@ RUN_FIREWALL_RULES=0
 # You need root permissions to run this script.
 if [[ "${UID}" != '0' ]]; then
     echo '> You need to become root to run this script.'
+    echo '> Aborting.'
     exit 1
 fi
 
-# Check if SeLinux is enabled on system.
+# Function that checks if required binary exists and installs it if necessary.
+ENSURE_PACKAGE () {
+    REQUIRED_BINARY=$(basename "${1}")
+    REPO_PACKAGES="${*:2}"
+
+    if [[ "${REQUIRED_BINARY}" != '-' ]]; then
+        [[ -n "${REPO_PACKAGES}" ]] || REPO_PACKAGES="${REQUIRED_BINARY}"
+
+        if command -v "${REQUIRED_BINARY}" 1> /dev/null; then
+            REPO_PACKAGES=''
+        fi
+    fi
+
+    [[ -n "${REPO_PACKAGES}" ]] || return
+
+    if [[ "${REPO_REFRESHED}" == '0' ]]; then
+        echo '> Refreshing package repository.'
+        dnf check-update 1> /dev/null
+        REPO_REFRESHED=1
+    fi
+
+    for REPO_PACKAGE in ${REPO_PACKAGES}
+    do
+        dnf install -y "${REPO_PACKAGE}"
+    done
+}
+
+# Variable that keeps track if repository is already refreshed.
+REPO_REFRESHED=0
+
+# Check if SELinux is enabled on system.
 SELINUX_ENABLED=0
-which selinuxenabled 1> /dev/null 2>&1
 
-if [[ "${?}" == '0' ]]; then
-    selinuxenabled
-
-    if [[ "${?}" == 0 ]]; then
+if command -v selinuxenabled 1> /dev/null 2>&1; then
+    if selinuxenabled; then
         SELINUX_ENABLED=1
     fi
 fi
 
-# Install packages.
-dnf install -y \
-    nfs-utils \
-    nfs4-acl-tools
+# Install packages and dependencies if necessary.
+ENSURE_PACKAGE 'mount.nfs' 'nfs-utils'
+ENSURE_PACKAGE 'fs4_getfacl' 'nfs4-acl-tools'
 
 # Make sure NFS share directory exists.
 if [[ ! -d "${SHARE_DIR}" ]]; then
@@ -49,9 +77,9 @@ EOL
 exportfs -avr
 
 # Active firewall rules.
-if [[ "${RUN_FIREWALL_RULES}" = '1' ]]; then
+if [[ "${RUN_FIREWALL_RULES}" == '1' ]]; then
     # Make sure firewalld is installed.
-    dnf install -y firewalld
+    ENSURE_PACKAGE 'firewall-cmd' 'firewall-cmd'
 
     # Enable Firewalld service.
     systemctl enable firewalld
@@ -76,4 +104,7 @@ systemctl restart rpcbind
 # Enable NFS service.
 systemctl enable nfs-server
 systemctl restart nfs-server
+
+# Let user know that script has finished its job.
+echo '> Finished.'
 
