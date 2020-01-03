6610df0843d6c1ea4e489251856c8fadd448d2d3
Kevin Rudissaar <kevin.rudissaar@gmail.com>
2020-01-03 18:53:52 +0000

    Improved fedora-post-install/mariadb.sh script.

--- fedora-post-install/mariadb.sh
+++ fedora-post-install/mariadb.sh
@@ -1,21 +1,61 @@
 #!/usr/bin/env bash
 # This script installs MariaDB server on your machine.
 
+ENABLE_SERVICES=1
+
 # You need root permissions to run this script.
 if [[ "${UID}" != '0' ]]; then
     echo '> You need to become root to run this script.'
+    echo '> Aborting.'
     exit 1
 fi
 
+# Function that checks if required binary exists and installs it if necessary.
+ENSURE_PACKAGE () {
+    REQUIRED_BINARY=$(basename "${1}")
+    REPO_PACKAGES="${*:2}"
+
+    if [[ "${REQUIRED_BINARY}" != '-' ]]; then
+        [[ -n "${REPO_PACKAGES}" ]] || REPO_PACKAGES="${REQUIRED_BINARY}"
+
+        if command -v "${REQUIRED_BINARY}" 1> /dev/null; then
+            REPO_PACKAGES=''
+        fi
+    fi
+
+    [[ -n "${REPO_PACKAGES}" ]] || return
+
+    if [[ "${REPO_REFRESHED}" == '0' ]]; then
+        echo '> Refreshing package repository.'
+        dnf check-update 1> /dev/null
+        REPO_REFRESHED=1
+    fi
+
+    for REPO_PACKAGE in ${REPO_PACKAGES}
+    do
+        dnf install -y "${REPO_PACKAGE}"
+    done
+}
+
+# Variable that keeps track if repository is already refreshed.
+REPO_REFRESHED=0
+
 # Install mariadb.
-dnf install -y \
-    mariadb \
-    mariadb-server
+ENSURE_PACKAGE 'mariadb-service-convert' 'mysql' 'mariadb-server'
 
-# Enable it.
-systemctl enable mariadb
+# Post install.
 systemctl start mariadb
-
-# Configure service.
 mysql_secure_installation
+
+# Configuring service.
+if [[ "${ENABLE_SERVICES}" == '1' ]]; then
+    systemctl enable mariadb
+    systemctl restart mariadb
+else
+    systemctl disable mariadb
+    systemctl stop mariadb
+fi
+
+# Let user know that script has finished its job.
+echo '> Finished.'
 
