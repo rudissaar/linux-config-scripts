b7f2e0114f9049a9691b9c8295232dd2f450f057
Kevin Rudissaar <kevin.rudissaar@gmail.com>
2020-01-04 06:57:36 +0000

    Improved fedora-post-install/onionshare.sh script.

--- fedora-post-install/onionshare.sh
+++ fedora-post-install/onionshare.sh
@@ -3,24 +3,54 @@
 # Script also enables you to prevent nautilus package from installing.
 
 # Option that helps you to prevent nautilus package from installing, it is useful if you are using some other
-# desktop environment than Gnome.
+# desktop environment than GNOME.
 WITHOUT_NAUTILUS=1
 
 # You need root permissions to run this script.
 if [[ "${UID}" != '0' ]]; then
     echo '> You need to become root to run this script.'
+    echo '> Aborting.'
     exit 1
 fi
 
+# Function that checks if required binary exists and installs it if necessary.
+ENSURE_PACKAGE () {
+    REQUIRED_BINARY=$(basename "${1}")
+    REPO_PACKAGES="${*:2}"
+
+    if [[ "${REQUIRED_BINARY}" != '-' ]]; then
+        [[ -n "${REPO_PACKAGES}" ]] || REPO_PACKAGES="${REQUIRED_BINARY}"
+
+        if command -v "${REQUIRED_BINARY}" 1> /dev/null; then
+            REPO_PACKAGES=''
+        fi
+    fi
+
+    [[ -n "${REPO_PACKAGES}" ]] || return
+
+    if [[ "${REPO_REFRESHED}" == '0' ]]; then
+        echo '> Refreshing package repository.'
+        dnf check-update 1> /dev/null
+        REPO_REFRESHED=1
+    fi
+
+    for REPO_PACKAGE in ${REPO_PACKAGES}
+    do
+        dnf install -y "${REPO_PACKAGE}"
+    done
+}
+
+# Variable that keeps track if repository is already refreshed.
+REPO_REFRESHED=0
+
 # Install required packages.
-dnf install -y \
-    coreutils \
-    curl \
-    jq \
-    rpm-build \
-    sed \
-    tar \
-    wget
+ENSURE_PACKAGE '-' 'coreutils'
+ENSURE_PACKAGE 'curl'
+ENSURE_PACKAGE 'jq'
+ENSURE_PACKAGE 'rpmbuild' 'rpm-build'
+ENSURE_PACKAGE 'sed'
+ENSURE_PACKAGE 'tar'
+ENSURE_PACKAGE 'wget'
 
 # Fetch URL of the latest release by using Github API.
 URL="$(curl -s "https://api.github.com/repos/micahflee/onionshare/tags" | jq .[0].tarball_url | tr -d '"')"
@@ -65,4 +95,6 @@ fi
 # Cleanup
 rm -rf "${TARBALL}" "/tmp/onionshare-${TMP_DATE}"
 
+# Let user know that script has finished its job.
 echo '> Finished.'
+
