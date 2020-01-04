4dd1eb8456a147bcca027d6e2efdf3be4f0752e4
Kevin Rudissaar <kevin.rudissaar@gmail.com>
2020-01-04 06:41:23 +0000

    Improved fedora-post-install/nouveau-off.sh script.

--- fedora-post-install/nouveau-off.sh
+++ fedora-post-install/nouveau-off.sh
@@ -1,10 +1,46 @@
 #!/usr/bin/env bash
+# Script that disables nouveau module in kernel.
 
 # You need root permissions to run this script.
 if [[ "${UID}" != "0" ]]; then
     echo '> You need to become root before you can run this script.'
+    echo '> Aborting.'
     exit 1
 fi
+
+# Function that checks if required binary exists and installs it if necessary.
+ENSURE_PACKAGE () {
+    REQUIRED_BINARY=$(basename "${1}")
+    REPO_PACKAGES="${*:2}"
+
+    if [[ "${REQUIRED_BINARY}" != '-' ]]; then
+        [[ -n "${REPO_PACKAGES}" ]] || REPO_PACKAGES="${REQUIRED_BINARY}"
+
+        if command -v "${REQUIRED_BINARY}" 1> /dev/null; then
+            REPO_PACKAGES=''
+        fi
+    fi
+
+    [[ -n "${REPO_PACKAGES}" ]] || return
+
+    if [[ "${REPO_REFRESHED}" == '0' ]]; then
+        echo '> Refreshing package repository.'
+        dnf check-update 1> /dev/null
+        REPO_REFRESHED=1
+    fi
+
+    for REPO_PACKAGE in ${REPO_PACKAGES}
+    do
+        dnf install -y "${REPO_PACKAGE}"
+    done
+}
+
+# Variable that keeps track if repository is already refreshed.
+REPO_REFRESHED=0
+
+# Install dependencies.
+ENSURE_PACKAGE 'grep'
+ENSURE_PACKAGE 'sed'
 
 # Remove Xorg package for Nouveau.
 dnf remove -y xorg-x11-drv-nouveau*
@@ -30,3 +66,7 @@ fi
 
 # Update GRUB. 
 grub2-mkconfig -o /boot/efi/EFI/fedora/grub.cfg
+
+# Let user know that script has finished its job.
+echo '> Finished.'
+
