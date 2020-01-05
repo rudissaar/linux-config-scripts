f67c3adbbb9b4c386db32ceddaf75bcb808c93e3
Kevin Rudissaar <kevin.rudissaar@gmail.com>
2020-01-05 12:23:55 +0000

    Improved fedora-post-install/openvpn.sh script.

--- fedora-post-install/openvpn.sh
+++ fedora-post-install/openvpn.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
+# Script that installs and configures OpenVPN server on current system.
 
 OPENVPN_SERVER_DIR='/etc/openvpn/server'
 
@@ -22,12 +23,47 @@ RUN_FIREWALL_RULES=0
 # You need root permissions to run this script.
 if [[ "${UID}" != '0' ]]; then
     echo '> You need to become root to run this script.'
+    echo '> Aborting.'
     exit 1
 fi
 
+# Function that checks if required binary exists and installs it if necessary.
+ENSURE_PACKAGE () {
+    REQUIRED_BINARY=$(basename "${1}")
+    REPO_PACKAGES="${*:2}"
+
+    if [[ "${REQUIRED_BINARY}" != '-' ]]; then
+        [[ -n "${REPO_PACKAGES}" ]] || REPO_PACKAGES="${REQUIRED_BINARY}"
+
+        if command -v "${REQUIRED_BINARY}" 1> /dev/null; then
+            REPO_PACKAGES=''
+        fi
+    fi
+
+    [[ -n "${REPO_PACKAGES}" ]] || return
+
+    if [[ "${REPO_REFRESHED}" == '0' ]]; then
+        echo '> Refreshing package repository.'
+        dnf check-update 1> /dev/null
+        REPO_REFRESHED=1
+    fi
+
+    for REPO_PACKAGE in ${REPO_PACKAGES}
+    do
+        dnf install -y "${REPO_PACKAGE}"
+    done
+}
+
+# Variable that keeps track if repository is already refreshed.
+REPO_REFRESHED=0
+
 # Install packages.
-dnf update -y
-dnf install -y openvpn firewalld easy-rsa openssl net-tools sed
+ENSURE_PACKAGE 'openvpn'
+ENSURE_PACKAGE 'firewall-cmd' 'firewalld'
+ENSURE_PACKAGE '-' 'easy-rsa'
+ENSURE_PACKAGE 'openssl'
+ENSURE_PACKAGE 'route' 'net-tools'
+ENSURE_PACKAGE 'sed'
 
 # Make sure OpenVPN server directory exists.
 if [[ ! -d "${OPENVPN_SERVER_DIR}" ]]; then
@@ -198,6 +234,7 @@ if [[ -z "${GATEWAY_INTERFACE}" ]]; then
 
     if [[ -z "${GATEWAY_INTERFACE}" ]]; then
         echo '> Unable to identify default Network Interface, please define it manually.'
+        echo '> Aborting.'
         exit 1
     fi
 fi
@@ -231,3 +268,7 @@ fi
 # Enable OpenVPN service.
 systemctl enable openvpn-server@server.service
 systemctl restart openvpn-server@server.service
+
+# Let user know that script has finished its job.
+echo '> Finished.'
+
