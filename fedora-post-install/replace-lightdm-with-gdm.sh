54b3240355e0f9537055b528938311391db49866
Kevin Rudissaar <kevin.rudissaar@gmail.com>
2020-01-09 20:45:13 +0000

    Improved fedora-post-install/replace-lightdm-with-gdm.sh script.

--- fedora-post-install/replace-lightdm-with-gdm.sh
+++ fedora-post-install/replace-lightdm-with-gdm.sh
@@ -4,21 +4,50 @@
 # You need root permissions to run this script.
 if [[ "${UID}" != '0' ]]; then
     echo '> You need to become root to run this script.'
+    echo '> Aborting.'
     exit 1
 fi
 
+# Function that checks if required binary exists and installs it if necessary.
+ENSURE_PACKAGE () {
+    REQUIRED_BINARY=$(basename "${1}")
+    REPO_PACKAGES="${*:2}"
+
+    if [[ "${REQUIRED_BINARY}" != '-' ]]; then
+        [[ -n "${REPO_PACKAGES}" ]] || REPO_PACKAGES="${REQUIRED_BINARY}"
+
+        if command -v "${REQUIRED_BINARY}" 1> /dev/null; then
+            REPO_PACKAGES=''
+        fi
+    fi
+
+    [[ -n "${REPO_PACKAGES}" ]] || return
+
+    if [[ "${REPO_REFRESHED}" == '0' ]]; then
+        echo '> Refreshing package repository.'
+        dnf check-update 1> /dev/null
+        REPO_REFRESHED=1
+    fi
+
+    for REPO_PACKAGE in ${REPO_PACKAGES}
+    do
+        dnf install -y "${REPO_PACKAGE}"
+    done
+}
+
+# Variable that keeps track if repository is already refreshed.
+REPO_REFRESHED=0
+
 # Install packages.
-dnf update --refresh
-dnf install -y gdm
+ENSURE_PACKAGE 'gdm'
 
 # Disable LightDM.
 systemctl disable lightdm
 
 # Enable GDM.
-systemctl enable gdm
 
 # Block that perform replacing display manager.
-if [[ "${?}" != '0' ]]; then
+if ! systemctl enable gdm; then
     echo '> Replacing LightDM with GDM failed.'
     echo '> Re-enabling LightDM and removing GDM package.'
     
