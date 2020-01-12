566d772109416f6f06c47230833c707df6a604b3
Kevin Rudissaar <kevin.rudissaar@gmail.com>
2020-01-12 02:43:58 +0000

    Improved fedora-post-install/selinux-off.sh script.

--- fedora-post-install/selinux-off.sh
+++ fedora-post-install/selinux-off.sh
@@ -4,28 +4,54 @@
 # You need root permissions to run this script.
 if [[ "${UID}" != '0' ]]; then
     echo '> You need to become root to run this script.'
+    echo '> Aborting.'
     exit 1
 fi
 
-# Check if SELinux is enabled.
-selinuxenabled
+# Function that checks if required binary exists and installs it if necessary.
+ENSURE_PACKAGE () {
+    REQUIRED_BINARY=$(basename "${1}")
+    REPO_PACKAGES="${*:2}"
 
-# Perform actions.
-if [[ "${?}" == '0' ]]; then
+    if [[ "${REQUIRED_BINARY}" != '-' ]]; then
+        [[ -n "${REPO_PACKAGES}" ]] || REPO_PACKAGES="${REQUIRED_BINARY}"
+
+        if command -v "${REQUIRED_BINARY}" 1> /dev/null; then
+            REPO_PACKAGES=''
+        fi
+    fi
+
+    [[ -n "${REPO_PACKAGES}" ]] || return
+
+    if [[ "${REPO_REFRESHED}" == '0' ]]; then
+        echo '> Refreshing package repository.'
+        dnf check-update 1> /dev/null
+        REPO_REFRESHED=1
+    fi
+
+    for REPO_PACKAGE in ${REPO_PACKAGES}
+    do
+        dnf install -y "${REPO_PACKAGE}"
+    done
+}
+
+# Variable that keeps track if repository is already refreshed.
+REPO_REFRESHED=0
+
+if selinuxenabled; then
+    # Install packages if necessary.
+    ENSURE_PACKAGE 'sed'
+
     # Disable SELinux in runtime.
     echo -n 0 > /sys/fs/selinux/enforce
-
-    # Make sure that sed binary is installed, we'll need this on next step.
-    which sed 1> /dev/null 2>&1
-    [[ "${?}" == '0' ]] || dnf install -y sed
 
     # Disable SELinux from config.
     sed -i 's/SELINUX=enforcing/SELINUX=disabled/g' /etc/selinux/config
 fi
 
 # Remove packages.
-dnf remove -y selinux* setroubleshoot-server
+dnf remove -y selinux*
 
-# Let user know that script has finished it's job.
+# Let user know that script has finished its job.
 echo '> Finished.'
 
