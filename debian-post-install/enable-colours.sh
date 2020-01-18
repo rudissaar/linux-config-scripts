57d27efc72a55f3a3f07cdedd946b32163b5beee
Kevin Rudissaar <kevin.rudissaar@gmail.com>
2020-01-18 18:40:27 +0000

    Improved debian-post-install/enable-colours.sh script.

--- debian-post-install/enable-colours.sh
+++ debian-post-install/enable-colours.sh
@@ -3,40 +3,62 @@
 # You need root permissions to run this script.
 if [[ "${UID}" != '0' ]]; then
     echo '> You need to become root to run this script.'
+    echo '> Aborting.'
     exit 1
 fi
 
-# Check if dircolors is installed.
-which dircolors 1>/dev/null 2>&1
+# Function that checks if required binary exists and installs it if necessary.
+ENSURE_PACKAGE () {
+    REQUIRED_BINARY=$(basename "${1}")
+    REPO_PACKAGES="${*:2}"
 
-# Install package that contains dircolors.
-if [[ "${?}" != '0' ]]; then
-    apt-get update -y
-    apt-get install -y coreutils
-fi
+    if [[ "${REQUIRED_BINARY}" != '-' ]]; then
+        [[ -n "${REPO_PACKAGES}" ]] || REPO_PACKAGES="${REQUIRED_BINARY}"
 
-grep -Fq "export LS_OPTIONS='--color=auto'" /etc/bash.bashrc
+        if command -v "${REQUIRED_BINARY}" 1> /dev/null; then
+            REPO_PACKAGES=''
+        fi
+    fi
 
-if [[ "${?}" != '0' ]]; then
+    [[ -n "${REPO_PACKAGES}" ]] || return
+
+    if [[ "${REPO_REFRESHED}" == '0' ]]; then
+        echo '> Refreshing package repository.'
+        apt-get update 1> /dev/null
+        REPO_REFRESHED=1
+    fi
+
+    for REPO_PACKAGE in ${REPO_PACKAGES}
+    do
+        apt-get install -y "${REPO_PACKAGE}"
+    done
+}
+
+# Variable that keeps track if repository is already refreshed.
+REPO_REFRESHED=0
+
+# Install dependencies if necessary.
+ENSURE_PACKAGE 'dircolors' 'coreutils'
+ENSURE_PACKAGE 'grep'
+
+# Configuration.
+if ! grep -Fq "export LS_OPTIONS='--color=auto'" /etc/bash.bashrc; then
     echo "export LS_OPTIONS='--color=auto'" >> /etc/bash.bashrc
 fi
 
-grep -Fq "eval \$(dircolors)" /etc/bash.bashrc
-
-if [[ "${?}" != '0' ]]; then
+if ! grep -Fq "eval \$(dircolors)" /etc/bash.bashrc; then
     echo "eval \$(dircolors)" >> /etc/bash.bashrc
 fi
 
-grep -Fq "alias ls='ls \${LS_OPTIONS}'" /etc/bash.bashrc
-
-if [[ "${?}" != '0' ]]; then
+if ! grep -Fq "alias ls='ls \${LS_OPTIONS}'" /etc/bash.bashrc; then
     echo "alias ls='ls \${LS_OPTIONS}'" >> /etc/bash.bashrc
 fi
 
-grep -Fq "alias grep='grep --color=auto'" /etc/bash.bashrc
-
-if [[ "${?}" != '0' ]]; then
+if ! grep -Fq "alias grep='grep --color=auto'" /etc/bash.bashrc; then
     echo "alias grep='grep --color=auto'" >> /etc/bash.bashrc
 fi
 
+
+# Let user know that script has finished its job.
 echo '> Finished.'
+
