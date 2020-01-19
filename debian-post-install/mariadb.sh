b432e04f639adf895d52da3a3f16231629e67ee3
Kevin Rudissaar <kevin.rudissaar@gmail.com>
2020-01-19 21:34:56 +0000

    Improved debian-post-install/mariadb.sh script.

--- debian-post-install/mariadb.sh
+++ debian-post-install/mariadb.sh
@@ -2,16 +2,51 @@
 
 if [[ "${UID}" != '0' ]]; then
     echo '> You need to become root to run this script.'
+    echo '> Aborting.'
     exit 1
 fi
 
+# Function that checks if required binary exists and installs it if necessary.
+ENSURE_PACKAGE () {
+    REQUIRED_BINARY=$(basename "${1}")
+    REPO_PACKAGES="${*:2}"
+
+    if [[ "${REQUIRED_BINARY}" != '-' ]]; then
+        [[ -n "${REPO_PACKAGES}" ]] || REPO_PACKAGES="${REQUIRED_BINARY}"
+
+        if command -v "${REQUIRED_BINARY}" 1> /dev/null; then
+            REPO_PACKAGES=''
+        fi
+    fi
+
+    [[ -n "${REPO_PACKAGES}" ]] || return
+
+    if [[ "${REPO_REFRESHED}" == '0' ]]; then
+        echo '> Refreshing package repository.'
+        apt-get update 1> /dev/null
+        REPO_REFRESHED=1
+    fi
+
+    for REPO_PACKAGE in ${REPO_PACKAGES}
+    do
+        apt-get install -y "${REPO_PACKAGE}"
+    done
+}
+
+# Variable that keeps track if repository is already refreshed.
+REPO_REFRESHED=0
+
 # Install packages.
-apt-get update -y
-apt-get install -y mariadb-common mariadb-server mariadb-client
+ENSURE_PACKAGE '-' 'mariadb-common' 'mariadb-server' 'mariadb-client'
 
 # Configure server.
+systectl start mariadb
 mysql_secure_installation
 
 # Making absolutely sure that service will be started on boot.
 systemctl enable mariadb
-systemctl start mariadb
+systemctl restart mariadb
+
+# Let user know that script has finished its job.
+echo '> Finished.'
+
