4f50e3a117cd666e49608c7ffe3a3aa620e43e59
Kevin Rudissaar <kevin.rudissaar@gmail.com>
2020-01-22 19:55:24 +0000

    Improved debian-post-install/vim.sh script.

--- debian-post-install/vim.sh
+++ debian-post-install/vim.sh
@@ -1,72 +1,103 @@
 #!/usr/bin/env bash
+# Script that installs and configures vim editor on current system.
 
 OVERWRITE_USERS_VIMRC=1
 
+# You need root permissions to run this script.
 if [[ "${UID}" != '0' ]]; then
     echo '> You need to become root to run this script.'
+    echo '> Aborting.'
     exit 1
 fi
 
-# Install packages.
-apt-get update
-apt-get install -y \
-    vim
+# Function that checks if required binary exists and installs it if necessary.
+ENSURE_PACKAGE () {
+    REQUIRED_BINARY=$(basename "${1}")
+    REPO_PACKAGES="${*:2}"
+
+    if [[ "${REQUIRED_BINARY}" != '-' ]]; then
+        [[ -n "${REPO_PACKAGES}" ]] || REPO_PACKAGES="${REQUIRED_BINARY}"
+
+        if command -v "${REQUIRED_BINARY}" 1> /dev/null; then
+            REPO_PACKAGES=''
+        fi
+    fi
+
+    [[ -n "${REPO_PACKAGES}" ]] || return
+
+    if [[ "${REPO_REFRESHED}" == '0' ]]; then
+        echo '> Refreshing package repository.'
+        dnf check-update 1> /dev/null
+        REPO_REFRESHED=1
+    fi
+
+    for REPO_PACKAGE in ${REPO_PACKAGES}
+    do
+        dnf install -y "${REPO_PACKAGE}"
+    done
+}
+
+# Variable that keeps track if repository is already refreshed.
+REPO_REFRESHED=0
+
+# Install dependencies if necessary.
+ENSURE_PACKAGE 'vim'
+ENSURE_PACKAGE 'grep'
+ENSURE_PACKAGE 'awk' 'gawk'
 
 # Replace vi command with vim.
-grep -Fq 'alias vi=' /etc/bash.bashrc
-
-if [[ "${?}" != '0' ]]; then
+if ! grep -Fq 'alias vi=' /etc/bash.bashrc; then
     echo >> /etc/bash.bashrc
     echo "alias vi='vim'" >> /etc/bash.bashrc
 fi
 
+# Create .vimrc file in /etc/skel directory.
 if [[ ! -f /etc/skel/.vimrc ]]; then
     touch /etc/skel/.vimrc
 fi
 
-# Enable syntax.
-grep -Fq 'syntax on' /etc/skel/.vimrc
+# Create .gvimrc file in /etc/skel directory.
+if [[ ! -f /etc/skel/.gvimrc ]]; then
+    touch /etc/skel/.gvimrc
+fi
 
-if [[ "${?}" != '0' ]]; then
+# Enable syntax.
+if ! grep -Fq 'syntax on' /etc/skel/.vimrc; then
     echo 'syntax on' >> /etc/skel/.vimrc
 fi
 
 # Display line numbers.
-grep -Fq 'set number' /etc/skel/.vimrc
-
-if [[ "${?}" != '0' ]]; then
+if ! grep -Fq 'set number' /etc/skel/.vimrc; then
     echo 'set number' >> /etc/skel/.vimrc
 fi
 
 # Disable visual mode.
-grep -Fq 'set mouse-=a' /etc/skel/.vimrc
-
-if [[ "${?}" != '0' ]]; then
+if ! grep -Fq 'set mouse-=a' /etc/skel/.vimrc; then
     echo 'set mouse-=a' >> /etc/skel/.vimrc
 fi
 
-# Sset size of tab.
-grep -Fq 'set tabstop=4' /etc/skel/.vimrc
-
-if [[ "${?}" != '0' ]]; then
+# Set size of tab.
+if ! grep -Fq 'set tabstop=4' /etc/skel/.vimrc; then
     echo 'set tabstop=4' >> /etc/skel/.vimrc
 fi
 
 # Use spaces instead of tabs.
-grep -Fq 'set expandtab' /etc/skel/.vimrc
-
-if [[ "${?}" != '0' ]]; then
+if ! grep -Fq 'set expandtab' /etc/skel/.vimrc; then
     echo 'set expandtab' >> /etc/skel/.vimrc
 fi
 
 # Set size of indent.
-grep -Fq 'set shiftwidth=4' /etc/skel/.vimrc
-
-if [[ "${?}" != '0' ]]; then
+if ! grep -Fq 'set shiftwidth=4' /etc/skel/.vimrc; then
     echo 'set shiftwidth=4' >> /etc/skel/.vimrc
 fi
 
-if [[ "${OVERWRITE_USERS_VIMRC}" = '1' ]]; then
+# Enable visual mode for gvim.
+if ! grep -Fq 'set mouse=a' /etc/skel/.gvimrc; then
+    echo 'set mouse=a' >> /etc/skel/.gvimrc
+fi
+
+# Block that populates user directories with generated .vimrc file.
+if [[ "${OVERWRITE_USERS_VIMRC}" == '1' ]]; then
     cp /etc/skel/.vimrc "${HOME}/.vimrc"
 
     if [[ -f /etc/login.defs ]]; then
@@ -76,13 +107,15 @@ if [[ "${OVERWRITE_USERS_VIMRC}" = '1' ]]; then
 
         for USERNAME in ${USERNAMES}; do
             HOME_DIR=$(eval echo "~${USERNAME}")
-	        cp /etc/skel/.vimrc "${HOME_DIR}/.vimrc"
-            chown ${USERNAME}:${USERNAME} "${HOME_DIR}/.vimrc"
+            cp /etc/skel/.vimrc "${HOME_DIR}/.vimrc"
+            cp /etc/skel/.gvimrc "${HOME_DIR}/.gvimrc"
+            chown "${USERNAME}":"${USERNAME}" "${HOME_DIR}/.vimrc" "${HOME_DIR}/.gvimrc"
         done
     else
         echo '> Unable to overwrite .vimrc for normal users, missing file: "/etc/login.defs".'
     fi
 fi
 
+# Let user know that script has finished its job.
 echo '> Finished.'
 
