f028bc02b2429b974a468aa96ae9c6dd0f7a3bbd
Kevin Rudissaar <kevin.rudissaar@gmail.com>
2020-03-24 22:39:43 +0000

    Improved fedora-post-install/qt5-devel.sh script.

--- fedora-post-install/qt5-devel.sh
+++ fedora-post-install/qt5-devel.sh
@@ -37,12 +37,23 @@ ENSURE_PACKAGE () {
 
 # Install packages.
 ENSURE_PACKAGE '-' 'qt5-devel' 'qt5-qtbase-devel'
+ENSURE_PACKAGE 'grep'
+ENSURE_PACKAGE 'sed'
 
-# link qmake binary.
+# Link qmake binary.
 if ! command -v qmake 1> /dev/null 2>&1; then
     if command -v qmake-qt5 1> /dev/null 2>&1; then
         ln -sf "$(command -v qmake-qt5)" /usr/local/bin/qmake
     fi
+fi
+
+# Enable debug messages.
+[[ -d /usr/share/qt5 ]] || mkdir -p /usr/share/qt5
+
+if ! grep -Fq '*.debug=' /usr/share/qt5/qtlogging.ini; then
+    echo '*.debug=true' >> /usr/share/qt5/qtlogging.ini
+else
+    sed -i -E 's/^\*\.debug=.*/*.debug=true/g' /usr/share/qt5/qtlogging.ini
 fi
 
 # Let user know that script has finished its job.
